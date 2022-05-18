f62e3f10454c205b027914fee7fdbf94fe7b4169
PeterArce96 <parce0496@gmail.com>
2022-05-17 20:37:50 -0500

    inicio y header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <header>
+            <h1>
+            Control Presupuestario
+            </h1>
+        </header>
+    )
+};
+
+export default Header;
+
+//parámetros que recibe un componente --> props
